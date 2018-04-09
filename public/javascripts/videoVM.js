7114dc6f2796f621727e8539ae9c6a0d3f3ad779
Landeryou <m_landeryou@tss-hr420-dm37.local>
2018-04-09 17:14:08 -0400

    node app for the video homework

--- public/javascripts/videoVM.js
+++ public/javascripts/videoVM.js
@@ -1,3 +1,28 @@
+Vue.component('review-stars', {
+  template : `<div class="star-rating">
+        <label class="star-rating-star" v-for="rating in ratings" 
+        :class="{'is-selected': ((value >= rating) && value != null), 'is-disabled': disabled}" 
+        v-on:click="updateValue(rating)" v-on:mouseover="updateValue(rating)" v-on:mouseout="updateValue(rating)">
+        <input class="star-rating star-rating-checkbox" type="radio" :value="rating" 
+        v-model="value" :disabled="disabled">★</label></div>`,
+
+  props: ['value', 'disabled'],
+
+  data : function() {
+    return {
+      ratings : [1, 2, 3, 4, 5]
+    };
+  },
+
+  methods : {
+    updateValue: function(value) {
+      if (!this.disabled) {
+        this.$emit('input', value);
+      }
+    }
+  }
+});
+
 var myVideoApp = {
 
   movieReviews(data, reviews){
@@ -11,8 +36,10 @@ var myVideoApp = {
     el : "#video",
     data : {
       reviews : [],
-      numStars : 3,
+      numStars : 0,
       review : ""
+
+      // testMessage : "testing to see if this works"
     },
     methods : {
       // do a post with all the new review stuff
@@ -20,27 +47,52 @@ var myVideoApp = {
         //fetch here
         let movieId = document.querySelector('.movId').textContent;
 
-        fetch('/api',{
-          method : 'post',
-          headers : {
-            'Accept' : 'applications/json, text-plain, */*',
-            'Content-type' : 'application/json'
-          },
-          body : JSON.stringify({
+        // fetch('/api',{
+        //   method : 'post',
+        //   headers : {
+        //     'Accept' : 'applications/json, text-plain, */*',
+        //     'Content-type' : 'application/json'
+        //   },
+        //   body : JSON.stringify({
+        //     id : movieId,
+        //     stars : this.numStars,
+        //     comment : this.review
+        //     })
+        //   })
+        //   .then((resp) => resp.json())
+        //   .then((data) => {
+        //     console.log(data);
+        //   })
+        //   .catch((error) =>{
+        //     console.log(error);
+        //   })
+        // }
+        
+        axios.post('/api', {
             id : movieId,
             stars : this.numStars,
             comment : this.review
-            })
           })
-          .then((resp) => resp.json())
-          .then((data) => {
-            console.log(data);
+          .then((response) => {
+            console.log(response);
           })
-          .catch((error) =>{
+          .catch((error) => {
             console.log(error);
-          })
-        }
+          });
+
+          //push things to the review array
+          
+          this.reviews.push({
+            comments_copy : this.review,
+            comments_rating : this.numStars,
+            comments_date : `${ new Date() }`
+          });
+
+          this.review = "";
+          this.numStars = 0;
       }
-    })
-};
+    }
+  })
+}
+
 myVideoApp.movieReviews(appData.movies);
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,15 @@
 var express = require('express');
-var connect = require('../utils/sqlConnect');
+var videoController = require('../controllers/videoAppController');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('get all the movies');
-  connect.query(`SELECT * FROM tbl_movies m, tbl_genre g, tbl_mov_genre mg WHERE m.movies_id = mg.movies_id AND g.genre_id = mg.genre_id`, (error, rows)=>{
-    if (error){
-      console.log(error);
-    }else{
-      res.render('home', {
-        defaultMovie : rows[Math.floor(Math.random() * rows.length)],
-        data : JSON.stringify(rows),
-        mainpage : true,
-        videopage : false
-      });
-    }
-  });
-});
+router.get('/', videoController.get_all_movies);
 
 // COMMENTS
-router.get('/movies/:id/:movie', (req, res) => {
-  console.log(req.params.id, req.params.movie);
-  connect.query(`SELECT * FROM tbl_comments WHERE comments_movie = "${req.params.id}"`, (error, rows) => {
-    if (error){
-      console.log(error);
-    }else{
-      console.log(rows);
-      res.render('moviePage', {
-        movie : req.params.id,
-        moviesrc : req.params.movie,
-        data : JSON.stringify(rows),
-        mainpage : false,
-        videopage : true
-      });
-    }
-  });
-});
+router.get('/movies/:id/:movie', videoController.get_one_movie);
+
+router.post('/api', videoController.post_new_review);
+  
+
 
 module.exports = router;
